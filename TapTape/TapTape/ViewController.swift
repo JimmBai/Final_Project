5a24c4a46de94f31c7cf9c5029203db9c3641d85
Jim Bai <jimbai@berkeley.edu>
2017-04-13 18:55:39 -0700

    initial commit

---
+++ TapTape/TapTape/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  TapTape
+//
+//  Created by Jim Bai on 2017/4/13.
+//  Copyright © 2017年 Jim Bai. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
